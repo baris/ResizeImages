e97563f254b769d9b2cbfcf482021ea38484998b
Baris Metin <bmetin@verivue.com>
2012-02-25 15:02:09 -0500

    Initial Commit

---
+++ FotoMoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FotoMoto
+//
+//  Created by Baris Metin on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FotoMoto/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  FotoMoto
+//
+//  Created by Baris Metin on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
